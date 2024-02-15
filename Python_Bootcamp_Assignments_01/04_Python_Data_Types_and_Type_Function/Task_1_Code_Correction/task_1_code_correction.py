98a3e5af573957566ef9540f8dd2704a171878d5
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-15 11:34:51 -0500

    assignment complete

---
+++ Python_Bootcamp_Assignments_01/04_Python_Data_Types_and_Type_Function/Task_1_Code_Correction/task_1_code_correction.py
@@ -0,0 +1,9 @@
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))  # output <'str'>
+print(type(variable_b))  # output <'int'>
+print(type(variable_c))  # output <'float'>
+print(type(variable_d))  # output <'bool'>
