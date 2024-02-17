c2117906b6ae40df577fd4eeb845fffc358f97ce
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-17 00:33:16 -0500

    Update task_1_grocery_store_math.py

--- Python_Bootcamp_Assignments_01/06_Arithmetic_Operations_in_Daily_Life/Task_1_Grocery_Store_Math/task_1_grocery_store_math.py
+++ Python_Bootcamp_Assignments_01/06_Arithmetic_Operations_in_Daily_Life/Task_1_Grocery_Store_Math/task_1_grocery_store_math.py
@@ -1,1 +1,5 @@
-# Python script
+item1_price = 2.5  # Price of item 1
+item2_price = 3.0  # Price of item 2
+item3_price = 4.5  # Price of item 3
+total_cost = item1_price + item2_price + item3_price
+print("Total Cost:", total_cost)
