78ddcfd842b0b3569d49e7ca59acf03e663be23d
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-17 00:33:36 -0500

    Update task_2_bank_interest.py

--- Python_Bootcamp_Assignments_01/06_Arithmetic_Operations_in_Daily_Life/Task_2_Bank_Interest/task_2_bank_interest.py
+++ Python_Bootcamp_Assignments_01/06_Arithmetic_Operations_in_Daily_Life/Task_2_Bank_Interest/task_2_bank_interest.py
@@ -1,1 +1,5 @@
-# Python script
+initial_amount = 1000  # Initial amount in the bank
+interest_rate = 0.05   # Yearly interest rate (5%)
+final_amount = initial_amount * (1 + interest_rate)
+print("
+
