619fb4e295a8bdcc5605066ab70f1161f5e3d9af
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-17 00:32:38 -0500

    Update task_1_code_correction.py

--- Python_Bootcamp_Assignments_01/05_Python_Dynamic_Typing_Practice/Task_1_Code_Correction/task_1_code_correction.py
+++ Python_Bootcamp_Assignments_01/05_Python_Dynamic_Typing_Practice/Task_1_Code_Correction/task_1_code_correction.py
@@ -1,1 +1,9 @@
-# Python script
+dynamic_variable = "This is a string"
+print(type(dynamic_variable))  # Output: <class 'str'>
+
+dynamic_variable = 100
+print(type(dynamic_variable))  # Output: <class 'int'>
+
+dynamic_variable = 25.5
+print(type(dynamic_variable))  # Output: <class 'float'>
+
