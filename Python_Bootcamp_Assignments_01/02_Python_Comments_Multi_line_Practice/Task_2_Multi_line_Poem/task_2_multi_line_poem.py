8759acb9293b16f31f07708466d3ba30b219a32b
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-17 00:30:24 -0500

    Update task_2_multi_line_poem.py

--- Python_Bootcamp_Assignments_01/02_Python_Comments_Multi_line_Practice/Task_2_Multi_line_Poem/task_2_multi_line_poem.py
+++ Python_Bootcamp_Assignments_01/02_Python_Comments_Multi_line_Practice/Task_2_Multi_line_Poem/task_2_multi_line_poem.py
@@ -1,1 +1,8 @@
-# Python script
+'''
+Python, in the realm of code you shine,
+With simplicity and grace, you're truly divine.
+Your libraries vast, your community strong,
+In solving problems, you help us along.
+Through loops and functions, your logic we weave,
+Creating wonders we can scarcely believe.
+'''
