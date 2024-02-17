420bfe6f6724836dcb746441f738e8bf1de31718
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-17 00:30:48 -0500

    Update task_3_combining_comments.py

--- Python_Bootcamp_Assignments_01/02_Python_Comments_Multi_line_Practice/Task_3_Combining_Comments/task_3_combining_comments.py
+++ Python_Bootcamp_Assignments_01/02_Python_Comments_Multi_line_Practice/Task_3_Combining_Comments/task_3_combining_comments.py
@@ -1,1 +1,8 @@
-# Python script
+# This line is from my single-line poem
+# In the world of programming, you have no peer.
+
+# This line is from my multi-line poem
+'''
+Through loops and functions, your logic we weave,
+'''
+
