6091f6d8ac7226d413bd46333c788c47acdeb07a
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-17 00:29:54 -0500

    Update task_1_poem_comments.py

--- Python_Bootcamp_Assignments_01/02_Python_Comments_Multi_line_Practice/Task_1_Poem_Comments/task_1_poem_comments.py
+++ Python_Bootcamp_Assignments_01/02_Python_Comments_Multi_line_Practice/Task_1_Poem_Comments/task_1_poem_comments.py
@@ -1,1 +1,6 @@
-# Python script
+# Python, oh Python, so clear and so neat
+# With every new challenge, you're hard to beat.
+# Your syntax so simple, your structure so clear,
+# In the world of programming, you have no peer.
+# With each line of code, our ideas take flight,
+# In the realm of logic, you're our guiding light.
