1485c02688b4831222ee16fa0b49f435a9452f3a
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2024-02-17 00:34:15 -0500

    Update task_3_area_and_perimeter.py

--- Python_Bootcamp_Assignments_01/06_Arithmetic_Operations_in_Daily_Life/Task_3_Area_and_Perimeter/task_3_area_and_perimeter.py
+++ Python_Bootcamp_Assignments_01/06_Arithmetic_Operations_in_Daily_Life/Task_3_Area_and_Perimeter/task_3_area_and_perimeter.py
@@ -1,1 +1,7 @@
-# Python script
+length = 10  # Length of the rectangle
+width = 5    # Width of the rectangle
+area = length * width
+perimeter = 2 * (length + width)
+print("Area:", area)
+print("Perimeter:", perimeter)
+
